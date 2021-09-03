5bce56671a9b8f3f4a1b84f2a98e63e8a1a55d81
Sahana Jayaprakash <sanajp044@gmail.com>
2021-09-03 15:15:37 +0530

    Java Learning

---
+++ workspace/day1/src/SampleOne.java
@@ -0,0 +1,15 @@
+
+public class SampleOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Java is better than C");
+	for(;;){
+		
+	}
+	}
+
+}
